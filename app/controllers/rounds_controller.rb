c05e0695a7691494f20a2b18ae6d12f4f3dbb6a6
Ian Gonnella <iangonnella@gmail.com>
2017-06-09 11:20:53 -0700

    Add some horrid css

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,9 +1,14 @@
 class RoundsController < ApplicationController
 
   def create
+    @round = Round.create(key: "key")
+    redirect_to round_path(@round)
   end
 
   def show
+    # respond_to do |format|
+    #     format.js {render layout: false}
+    # end
   end
 
   def destroy
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,15 @@
 class Round < ApplicationRecord
+  before_create :set_key
+
   has_many :restaurants
 
   validates :key, presence: true, uniqueness: true
+
+
+
+  def set_key
+    # if we want cap letters
+    # self.key = Array.new(8){rand(36).to_s(36)}.map {|y| y =~ /[^o]/ ? y.upcase : y }.join
+    self.key = Array.new(8){rand(36).to_s(36)}.join
+  end
 end
